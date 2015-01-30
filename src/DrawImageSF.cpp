aef145327ec3511c11b65b8dbabcb44baafd11f9
SanchezSobrino <sanchezsobrino@gmail.com>
2015-01-30 13:28:46 +0100

    Botón con mano añadido.
    Corregido bug que no permitía añadir mas de una imagen al documento.

--- src/DrawImageSF.cpp
+++ src/DrawImageSF.cpp
@@ -5,6 +5,8 @@
 
 namespace argosClient {
 
+  unsigned long long DrawImageSF::counter = 0;
+
   DrawImageSF::DrawImageSF()
     : ScriptFunction("Image_", "DrawImageSF"),
       _graphicComponentsManager(GraphicComponentsManager::getInstance()) {
@@ -12,11 +14,13 @@ namespace argosClient {
   }
 
   void DrawImageSF::_execute(const std::vector<std::string>& args, int id) {
-    _graphicComponentsManager.createImageFromFile(_name + std::to_string(id),
+    _graphicComponentsManager.createImageFromFile(_name + "_id:" + std::to_string(id) + "_num:" + std::to_string(counter),
                                                   args[0],
                                                   glm::vec3(getArgAsFloat(args[1]), getArgAsFloat(args[2]), getArgAsFloat(args[3])),
                                                   glm::vec2(getArgAsFloat(args[4]), getArgAsFloat(args[5]))
                                                   )->show(true);
+
+    (counter > 9999)?counter = 0:++counter;
   }
 
 }
